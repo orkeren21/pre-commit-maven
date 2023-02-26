0e94194e917e34c1ea73b6ae0e4882520b704f0e
Or Keren <okeren@salesforce.com>
2023-02-26 12:07:20 +0200

    refactoring

--- pre_commit_maven/maven_spotless_apply.py
+++ pre_commit_maven/maven_spotless_apply.py
@@ -4,14 +4,15 @@ import os.path
 from pre_commit_maven.utils import generic_main
 from pre_commit_maven.utils import shell
 from pre_commit_maven.utils.shell import ExecutionResult
+from pre_commit_maven.utils.maven import Colours
 
 CWD = os.getcwd()
 ENV = os.environ.copy()
+ADDED_FILTER = "A"
+MODIFIED_FILTER = "M"
 GREP_FILTER_AND_CLEAN = "grep .java$ | tr '\\n' \' \' | rev | cut -c 2- | rev"
-GIT_DIFF_MODIFIED_ONLY = f"git diff --cached --name-only --diff-filter=M | {GREP_FILTER_AND_CLEAN}"
-GIT_DIFF_ADDED_MODIFIED = f"git diff --cached --name-only --diff-filter=AM | {GREP_FILTER_AND_CLEAN}"
-DINCLUDES_ADDED_MODIFIED = f"-Dincludes=\'$(git diff --cached --name-only --diff-filter=AM | {GREP_FILTER_AND_CLEAN})\'"
-
+GIT_DIFF_MODIFIED_ONLY = f"git diff --cached --name-only --diff-filter={MODIFIED_FILTER} | {GREP_FILTER_AND_CLEAN}"
+GIT_DIFF_ADDED_MODIFIED = f"git diff --cached --name-only --diff-filter={ADDED_FILTER}{MODIFIED_FILTER} | {GREP_FILTER_AND_CLEAN}"
 
 
 def main(cwd=CWD, print_fn=print, execute_fn=generic_main.execute) -> int:
@@ -22,9 +23,11 @@ def auto_fix_and_add(previous_return_code, cwd=CWD):
     # Important that following command is MODIFIED filter only!
     result = ExecutionResult(previous_return_code, "", "")
     modified_files =  shell.execute_direct(GIT_DIFF_MODIFIED_ONLY, cwd=cwd, env=ENV)
-    print(f"ran modified files got {modified_files.stdout} and {modified_files.stderr}")
+    print(f"{Colours.OKBLUE} Spotless modified the following files: {modified_files.stdout} {Colours.ENDC}")
     if modified_files.stdout != '':
         shell.execute_direct("git add " + modified_files.stdout)
+        print(f"{Colours.BOLD} {Colours.OKGREEN} Testing {Colours.ENDC}")
+        print(f"{Colours.HEADER} {Colours.OKBLUE} Successfully Added Files {Colours.ENDC}")
         return result.return_code
     return result.return_code
 
