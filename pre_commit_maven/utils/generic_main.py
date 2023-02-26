c59d48710c37bf6800d74e40e4d49ee78bdb77a0
Or Keren <okeren@salesforce.com>
2023-02-26 08:30:27 +0200

    bug fix

--- pre_commit_maven/utils/generic_main.py
+++ pre_commit_maven/utils/generic_main.py
@@ -6,6 +6,7 @@ from pre_commit_maven.utils import maven
 def execute(goals: list, cwd: str, maven_helper=maven):
     execution_result = maven_helper.execute(goals, cwd)
     if execution_result.return_code != 0:
+        print(execution_result.stderr)
         maven_helper.print_error(execution_result)
 
     return execution_result.return_code
